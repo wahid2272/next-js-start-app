43256ca5e46f282f5fe5777442c2e1ce07b8d5b7
Wahiduzzaman Siddique <wahid2272@gmail.com>
2021-09-05 14:53:58 +0300

    index and new page created in pages folder

---
+++ pages/index.js
@@ -0,0 +1,5 @@
+function HomePage() {
+    return <h1>Welcome to Home the Page</h1>
+}
+
+export default HomePage;
---
+++ pages/news.js
@@ -0,0 +1,5 @@
+function NewsPage() {
+    return <h1>Welcome to News the Page</h1>
+}
+
+export default NewsPage;
